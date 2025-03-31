9369bda7d5941837b4a9e9f548303c4b06aefa5d
frnbrz <frnbrz@gmail.com>
2025-03-31 12:57:15 +0200

    add character list and view components with associated styles and interfaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CharacterListComponent } from './pages/character-list/character-list.component';
+import { CharacterViewComponent } from './pages/character-view/character-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CharacterListComponent,
+  },
+  {
+    path: 'character/:idCharacter',
+    component: CharacterViewComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
