9582929175551c10ff22e27a34648b40a02237f9
frnbrz <frnbrz@gmail.com>
2025-03-31 13:40:16 +0200

    refactor character view and card components for improved functionality and routing

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-character-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './character-card.component.html',
   styleUrl: './character-card.component.css',
 })
--- src/app/pages/character-view/character-view.component.ts
+++ src/app/pages/character-view/character-view.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { CharacterCardComponent } from '../../components/character-card/character-card.component';
+import { Character } from '../../interfaces/character.interface';
+import { CharactersService } from '../../services/characters.service';
+import { CharacterListComponent } from '../character-list/character-list.component';
 
 @Component({
   selector: 'app-character-view',
-  imports: [],
+  imports: [CharacterCardComponent],
   templateUrl: './character-view.component.html',
-  styleUrl: './character-view.component.css'
+  styleUrl: './character-view.component.css',
 })
 export class CharacterViewComponent {
+  @Input() idCharacter: string = '';
+  characterService = inject(CharactersService);
+  character: Character | undefined;
 
+  async ngOnInit() {
+    this.character = await this.characterService.getById(
+      Number(this.idCharacter)
+    );
+  }
 }
